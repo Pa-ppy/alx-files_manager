a113cc329acc02c5e892e2a8c2ffb85655d90410
Pa-ppy <rayjacks07@gmail.com>
2025-06-24 17:03:35 +0000

    Utils

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+
+const host = process.env.DB_HOST || "localhost";
+const port = process.env.DB_PORT || 27017;
+const dbName = process.env.DB_DATABASE || "files_manager";
+const url = `mongodb://${host}:${port}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client
+      .connect()
+      .then(() => {
+        this.db = this.client.db(dbName);
+      })
+      .catch((err) => {
+        console.error(`MongoDB connection error: ${err}`);
+      });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.db.collection("users").countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection("files").countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
