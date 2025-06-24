197d01b77b7a4a1fb73296587fdf4a13ee64dc01
Pa-ppy <rayjacks07@gmail.com>
2025-06-24 17:08:04 +0000

    Routes

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
+const router = express.Router();
+
+// Status and stats
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+// User management
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+// Authentication
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+// File management
+router.post('/files', FilesController.postUpload);
+
+export default router;
